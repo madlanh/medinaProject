dd710b762d71d0626907142bf2eec47ea5beb5ba
madlanh <116224003+madlanh@users.noreply.github.com>
2025-12-21 20:48:11 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,18 +5,11 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'kunci_rahasia_anda_yang_sangat_aman_dan_panjang'
-    
-    database_url = os.environ.get('DATABASE_URL')
-    
-    if database_url and database_url.startswith("postgres://"):
-        database_url = database_url.replace("postgres://", "postgresql://", 1)
-        
-    SQLALCHEMY_DATABASE_URI = database_url or (
+    SQLALCHEMY_DATABASE_URI = (
         f"mysql+mysqlconnector://{os.environ.get('DB_USERNAME', 'root')}:"
         f"{os.environ.get('DB_PASSWORD', '')}@"
         f"{os.environ.get('DB_HOST', 'localhost')}:"
         f"{os.environ.get('DB_PORT', '3306')}/"
         f"{os.environ.get('DB_NAME', 'medinadb')}"
     )
-    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
