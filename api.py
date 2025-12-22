3d217a73d6eb936f3b6fa127790d646e3b01579c
madlanh <116224003+madlanh@users.noreply.github.com>
2025-12-23 06:29:05 +0700

    Update api.py

--- api.py
+++ api.py
@@ -1,11 +1,5 @@
 from app import create_app
 
-import os
-print("Isi folder app/routes:", os.listdir('app/routes'))
-# Jika public ada, cek isinya
-if 'public' in os.listdir('app/routes'):
-    print("Isi folder app/routes/public:", os.listdir('app/routes/public'))
-
 app = create_app()
 
 if __name__ == '__main__':
