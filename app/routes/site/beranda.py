c6283ccebd1c5b814bc3742f7a2aa45aecbd0280
madlanh <116224003+madlanh@users.noreply.github.com>
2025-12-23 09:36:42 +0700

    Update and rename beranda.py to beranda.py

--- app/routes/site/beranda.py
+++ app/routes/site/beranda.py
@@ -1,7 +1,8 @@
 # Beranda (Homepage) Route
 from flask import render_template
 from app.models.models import Berita, Banner, Prestasi, AlumniTestimoni
-from app.routes.public import public_bp
+# from app.routes.public import public_bp
+from app.routes.site import site_bp as public_bp
 
 
 @public_bp.route('/')
