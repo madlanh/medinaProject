965f7a0519a4307886e1a550b0cbf8a5de81a54a
madlanh <116224003+madlanh@users.noreply.github.com>
2025-12-23 09:37:16 +0700

    Update and rename agenda.py to agenda.py

--- app/routes/site/agenda.py
+++ app/routes/site/agenda.py
@@ -1,7 +1,8 @@
 # Agenda Routes
 from flask import render_template
 from app.models.models import Agenda
-from app.routes.public import public_bp
+# from app.routes.public import public_bp
+from app.routes.site import site_bp as public_bp
 
 
 @public_bp.route('/agenda')
