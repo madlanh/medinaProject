3d297e0e070fd9b58489c550fc7f8d4c32f40918
madlanh <116224003+madlanh@users.noreply.github.com>
2025-12-26 08:48:34 +0700

    Update config.py

--- config.py
+++ config.py
@@ -17,3 +17,7 @@ class Config:
     )
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SUPABASE_URL = os.environ.get('SUPABASE_URL')
+    SUPABASE_KEY = os.environ.get('SUPABASE_KEY')
+    SUPABASE_BUCKET = 'images'
